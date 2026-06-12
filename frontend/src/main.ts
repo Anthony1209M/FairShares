7db1d416b29f8afd4f3cd70c35a7c2aab069a61e
Anthony Nivar Martinez <elincreible2009@gmail.com>
2026-06-12 15:43:35 -0400

    setup: initialize frontend with Vite and Tailwind

---
+++ frontend/src/main.ts
@@ -0,0 +1,12 @@
+import './style.css'
+import typescriptLogo from './assets/typescript.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+// document.querySelector<HTMLDivElement>('#app')!.innerHTML +=
+// `
+
+
+// `
+
+// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
