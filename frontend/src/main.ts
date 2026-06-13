6f4e6a584799eb8cc5c89d11a31ae9aee896c208
Anthony Nivar Martinez <elincreible2009@gmail.com>
2026-06-13 13:08:43 -0400

    feat: add login UI - inputs, show/hide password, box shadow and log in button

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,7 +1,7 @@
 import './style.css'
-import typescriptLogo from './assets/typescript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+// import typescriptLogo from './assets/typescript.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
 
 // document.querySelector<HTMLDivElement>('#app')!.innerHTML +=
 // `
@@ -10,3 +10,47 @@ import heroImg from './assets/hero.png'
 // `
 
 // setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+function togglePasswordVisibility(e: MouseEvent)
+{
+    const inputPass = document.querySelector<HTMLInputElement>('.input-pass');
+
+    if(inputPass?.type === "password")
+    {
+        inputPass.type = "text";
+    }
+    else if(inputPass?.type === "text")
+    {
+        inputPass.type = "password";
+    }
+    
+}
+
+function toggleIconVisibility(e: Event)
+{
+    const togglePasswordBtn = document.getElementById("togglePasswordBtn");
+    const input = document.querySelector<HTMLInputElement>('.input-pass')!;
+
+    if(input.value.length > 0)
+    {
+        togglePasswordBtn?.classList.remove("hidden")
+    }
+    else
+    {
+        togglePasswordBtn?.classList.add("hidden")
+    }
+    
+
+
+}
+
+
+const togglePasswordBtn = document.getElementById("togglePasswordBtn");
+const inputPass = document.querySelector('.input-pass');
+
+togglePasswordBtn?.addEventListener("click", togglePasswordVisibility);
+
+inputPass?.addEventListener("input", toggleIconVisibility);
+
+
+
